8d20b3581753dc2ba8153f1d912554ca6d3ac2e5
Nameer <nameer6g6@gmail.com>
2020-05-27 15:42:17 +0300

    finished adding users

--- apps/auction/lib/auction.ex
+++ apps/auction/lib/auction.ex
@@ -1,7 +1,7 @@
 defmodule Auction do
-  alias Auction.{Item}
+  alias Auction.{Repo, Item, User, Password}
 
-  @repo Auction.Repo
+  @repo Repo
 
   def list_items do
     @repo.all(Item)
@@ -30,4 +30,30 @@ defmodule Auction do
   end
 
   def new_item(), do: Item.changeset(%Item{})
+
+  def edit_item(id) do
+    get_item(id)
+    |> Item.changeset()
+  end
+
+  # User Section
+
+  def get_user(id), do: @repo.get!(User, id)
+
+  def new_user, do: User.changeset_with_password(%User{})
+
+  def insert_user(params) do
+    %User{}
+    |> User.changeset_with_password(params)
+    |> @repo.insert()
+  end
+
+  def get_user_by_username_and_password(username, password) do
+    with user when not is_nil(user) <- @repo.get_by(User, %{username: username}),
+         true <- Password.verify_with_hash(password, user.hashed_password) do
+      user
+    else
+      _ -> Password.dummy_verify()
+    end
+  end
 end
